72622e4054972ca4e19a0cf423ff32420219720b
BT_1N3_33@BKTECH.LOCAL <BT_1N3_33@BKTECH.LOCAL>
2019-01-03 14:45:41 -0500

    Initial commit

---
+++ src/SortingUtil.java
@@ -0,0 +1,12 @@
+public class SortingUtil {
+    public static void BubbleSort(int[]arr){
+        int swaps=0;
+        while (swaps>0){
+            swaps=1;
+            for (int i=0; i<arr.length-1; i++)
+            {
+                if (arr)
+            }
+        }
+    }
+}
